c2d7a382fc8ee1244370e0b202e8d79e1e07ed92
jumtech <despairing.clock+jumGithub@gmail.com>
2017-06-11 15:13:52 +0900

    add appendBotMessage

--- public/script.js
+++ public/script.js
@@ -6,9 +6,17 @@ const submit = (e) => {
     const str = $('.textarea').val();
     appendSelfMessage(str);
     $('.textarea').val('');
+    // fetchBotReply(str, function(reply) {
+    //     appendBotMessage(reply);
+    // });
 }
 
 const appendSelfMessage = (str) => {
     let message = '<li class="self"><div class="avatar"><img src="https://i.imgur.com/HYcn9xO.png" draggable="false"/></div><div class="msg"><p>'+str+'</p><time>20:18</time></div></li>'
+    $(".chat").append(message);
+}
+
+const appendBotMessage = (str) => {
+    let message = '<li class="other"><div class="avatar"><img src="https://i.imgur.com/DY6gND0.png" draggable="false"/></div><div class="msg"><p>'+str+'</p><time>20:18</time></div></li>'
     $(".chat").append(message);
 }
